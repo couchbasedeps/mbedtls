16f937ccf29ea2cea8a3ae44edc2a044620700f2
Callum Birks <callumbirks@protonmail.com>
2023-06-07 15:38:50 +0100

    Fix error

--- library/bignum.c
+++ library/bignum.c
@@ -1388,7 +1388,7 @@ void mpi_mul_hlp( size_t i,
                   mbedtls_mpi_uint *d,
                   mbedtls_mpi_uint b )
 {
-    mbedtls_mpi_uint c = 0, t = 0;
+    mbedtls_mpi_uint c = 0;
 
 #if defined(MULADDC_HUIT)
     for( ; i >= 8; i -= 8 )
@@ -1438,8 +1438,6 @@ void mpi_mul_hlp( size_t i,
         MULADDC_STOP
     }
 #endif /* MULADDC_HUIT */
-
-    t++;
 
     while( c != 0 )
     {
