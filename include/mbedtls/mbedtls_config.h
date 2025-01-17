06bcd5b2b1e862087eb48373b8536ea139754e7d
Jens Alfke <jens@couchbase.com>
2025-01-17 15:47:51 -0800

    Enable MBEDTLS_THREADING_C [CBL-6649]
    (cherry picked from commit 1178d25852b283665627e4a6bccfb1897e676099)

--- include/mbedtls/mbedtls_config.h
+++ include/mbedtls/mbedtls_config.h
@@ -2183,6 +2183,7 @@
  * Uncomment this to allow your own alternate threading implementation.
  */
 //#define MBEDTLS_THREADING_ALT
+// FIXME: Need to define MBEDTLS_THREADING_ALT on Windows and implement mutex functions --Jens
 
 /**
  * \def MBEDTLS_THREADING_PTHREAD
@@ -2193,7 +2194,9 @@
  *
  * Uncomment this to enable pthread mutexes.
  */
-//#define MBEDTLS_THREADING_PTHREAD
+#ifndef _WIN32
+#define MBEDTLS_THREADING_PTHREAD
+#endif
 
 /**
  * \def MBEDTLS_USE_PSA_CRYPTO
@@ -3784,7 +3787,9 @@
  *
  * Enable this layer to allow use of mutexes within Mbed TLS
  */
-//#define MBEDTLS_THREADING_C
+#ifndef _WIN32  // FIXME: Remove this when implementing threading for Windows --Jens
+#define MBEDTLS_THREADING_C
+#endif
 
 /**
  * \def MBEDTLS_TIMING_C
